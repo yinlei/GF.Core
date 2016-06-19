e85ce50dc680fc57fdbd1ed587ddef9d08d3d416
lion <xiaolige945@vip.qq.com>
2016-06-19 23:00:59 +0800

    添加Cragon.Game.Gateway工程和Cragon.Game.Cell工程。

---
+++ Cragon.Game.Cell/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cragon.Game.Cell
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cell Start");
+
+            Console.ReadKey();
+        }
+    }
+}
